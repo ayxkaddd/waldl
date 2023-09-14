49af33dddf5b8b3095189f81636c599f0779dc1c
x <bladeec1ty@protonmail.com>
2023-09-14 14:55:25 +0300

    update waldl.py replaced threading with concurrent.futures. added request headers so it wont fail to download every image

--- waldl.py
+++ waldl.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
 import os
 import sys
 import random
 import string
 import pathlib
 import requests
-import threading
+from concurrent.futures import ThreadPoolExecutor
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0',
+    'Accept': 'image/avif,image/webp,*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://wallhaven.cc/',
+}
 
 DOWNLOAD_DIR = f"{str(pathlib.Path.home())}/pics/walls"
 retry_list = []
@@ -23,7 +31,7 @@ def download_wallpaper(url):
     print(f"[***] Downloading {small_url}")
     with requests.Session() as session: 
         session.keep_alive = True
-        res = session.get(url, allow_redirects=True)
+        res = session.get(url, allow_redirects=True, headers=headers)
         if res.status_code == 200:
             download_path = f"{DOWNLOAD_DIR}/{generate_id()}{get_ext(url)}"
             open(download_path, 'wb').write(res.content)
@@ -37,9 +45,9 @@ def wallpaper_search_api(query):
     if query == "--random" or query == "-r":
         query_url = "https://wallhaven.cc/api/v1/search?sorting=random"
     else:
-        query_url = f"https://wallhaven.cc/api/v1/search?q={query}&?sorting=random" 
+        query_url = f"https://wallhaven.cc/api/v1/search?q={query}&sorting=random" 
     
-    res = requests.get(query_url)
+    res = requests.get(query_url, headers=headers)
     response = res.json()
     dl_links = []
     for wallpaper in response["data"]:
@@ -48,14 +56,25 @@ def wallpaper_search_api(query):
     return dl_links
 
 
+def download_unsucces(url):
+    res = requests.get(url, allow_redirects=True, headers=headers)
+    if res.status_code == 200:
+        download_path = f"{DOWNLOAD_DIR}/{generate_id()}{get_ext(url)}"
+        open(download_path, 'wb').write(res.content)
+        print(f"[+++] Downloading done of {url}")
+    else:
+        print(f"[!!!] Failed to download {url}.") 
+
+
 def retry_unsucces():
     global retry_list
-    print(f"[***] Retrying to download {len(retry_list)} unsucces downloaded wallpapers")
     if len(retry_list) == 0:
         print("[***] No need to retry!")
     else:
+        print(f"[***] Retrying to download {len(retry_list)} unsuccess downloaded wallpapers")
         for url in retry_list:
-            download_wallpaper(url)
+            print(url)
+            download_unsucces(url)
 
     retry_list = []
 
@@ -70,17 +89,11 @@ if __name__ == "__main__":
     query = sys.argv[1]
     wallpapers = wallpaper_search_api(query)
 
-    threads = []
+    maxtheads=5
 
-    for wallpaper in wallpapers:
-        t = threading.Thread(target=download_wallpaper, args=(wallpaper,))
-        t.start()
-        threads.append(t)
-
-
-    for t in threads:
-        t.join()
-
+    with ThreadPoolExecutor(max_workers=maxtheads) as executor:
+        for wallpaper in wallpapers:
+            executor.submit(download_wallpaper, wallpaper)
 
     retry_unsucces()
-    print(f"[+++] Download complite!")
+    print(f"[+++] Download is complete")
